85f4aef00599531dc49c3c7febf7c1c95b53567e
Niruase <Niruase@users.noreply.github.com>
2026-06-27 04:38:30 -0600

    Fix language detection

--- Source/CNPawnNames/HarmonyPatcher.cs
+++ Source/CNPawnNames/HarmonyPatcher.cs
@@ -9,12 +9,15 @@ public class HarmonyPatcher
 {
     static HarmonyPatcher()
     {
-        if (LanguageDatabase.activeLanguage?.folderName != "ChineseSimplified")
+        if (LanguageDatabase.activeLanguage?.folderName != "ChineseSimplified (简体中文)")
         {
             Log.Warning("[CNPawnNames] 未启用：当前语言不是简体中文。 Inactive: current language is not Simplified Chinese.");
+            Log.Warning($"[CNPawnNames] 当前语言：{LanguageDatabase.activeLanguage?.folderName ?? "未加载"}");
+
             return;
         }
 
         new Harmony("Niruase.CNPawnNames").PatchAll();
+        Log.Message("[CNPawnNames] 已启用。Loaded.");
     }
 }
